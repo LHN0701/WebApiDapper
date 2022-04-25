16e917562c34c15ada14488cf733b2c08c44754d
LHN0701 <93341386+LHN0701@users.noreply.github.com>
2022-04-25 16:56:04 +0700

    CRUD

--- WebApiDapper/Controllers/ProductController.cs
+++ WebApiDapper/Controllers/ProductController.cs
@@ -31,34 +31,78 @@ namespace WebApiDapper.Controllers
             {
                 if (conn.State == System.Data.ConnectionState.Closed)
                     conn.Open();
-                var result = await conn.QueryAsync<Product>("select Id, Sku, Price, DiscountPrice, ImageUrl, CreateAt, IsActive, ViewCount from Products", null, null, null, System.Data.CommandType.Text);
+                var result = await conn.QueryAsync<Product>("Get_Product_All", null, null, null, System.Data.CommandType.StoredProcedure);
                 return result;
             }
         }
 
         // GET api/<ProductController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<Product> Get(int id)
         {
-            return "value";
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                    conn.Open();
+                var parameters = new DynamicParameters();
+                parameters.Add("@id", id);
+                var result = await conn.QueryAsync<Product>("Get_Product_ById", parameters, null, null, System.Data.CommandType.StoredProcedure);
+                return result.Single();
+            }
         }
 
         // POST api/<ProductController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<int> Post([FromBody] Product product)
         {
+            int newId = 0;
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                    conn.Open();
+                var parameters = new DynamicParameters();
+                parameters.Add("@sku", product.Sku);
+                parameters.Add("@price", product.Price);
+                parameters.Add("@isActive", product.IsActive);
+                parameters.Add("@imageUrl", product.ImageUrl);
+                parameters.Add("@id", dbType: System.Data.DbType.Int32, direction: System.Data.ParameterDirection.Output);
+                var result = await conn.ExecuteAsync("Create_Product", parameters, null, null, System.Data.CommandType.StoredProcedure);
+
+                newId = parameters.Get<int>("@id");
+            }
+            return newId;
         }
 
         // PUT api/<ProductController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task Put(int id, [FromBody] Product product)
         {
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                    conn.Open();
+                var parameters = new DynamicParameters();
+                parameters.Add("@id", id);
+                parameters.Add("@sku", product.Sku);
+                parameters.Add("@price", product.Price);
+                parameters.Add("@isActive", product.IsActive);
+                parameters.Add("@imageUrl", product.ImageUrl);
+                await conn.ExecuteAsync("Update_Product", parameters, null, null, System.Data.CommandType.StoredProcedure);
+            }
         }
 
         // DELETE api/<ProductController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task Delete(int id)
         {
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                    conn.Open();
+                var parameters = new DynamicParameters();
+                parameters.Add("@id", id);
+                await conn.ExecuteAsync("Delete_Product_ById", parameters, null, null, System.Data.CommandType.StoredProcedure);
+            }
         }
     }
 }
